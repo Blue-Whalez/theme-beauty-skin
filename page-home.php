5a16944422d6960b9bb3ac68db350590bb8c6e30
TuanTQ <tquoctua97@gmail.com>
2021-03-15 23:36:53 +0700

    Hero Template

--- page-home.php
+++ page-home.php
@@ -1,18 +1,51 @@
 <?php /* Template Name: Home Template */ ?>
 <?php get_header(); ?>
 <main>
-        <section class="hero" style="background-image: url(https://wp.w3layouts.com/beauty-skin/wp-content/themes/beauty-skin/assets/images/banner.png);">
-            <div class="row">
-                <div class="col large-6">
-                    <h1>WELCOME TO <br> BEAUTY <span class="main-text">SKIN</span> </h1>
-            <div>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non.
-            </div>
-            <div class="buttons-banner mt-5">
-                <a class="btn">Reserver now</a> <a class="btn">Play Video</a>
-            </div>
-                </div>
-            </div>
-        </section>
-    </main>
+    <section class="hero" <?php 
+    $hero_image = get_field('hero_image');
+    if( $hero_image ) {
+      echo "style='background-image:url(" .esc_url($hero_image['url']) . "'";
+
+    }
+    ?>>
+      <div class="row">
+          <div class="col large-6">
+            <h1><?php the_field('hero_title'); ?></h1>
+            <?php 
+              $hero_description = get_field('hero_description');
+              if( $hero_description ) {
+                  echo "<div>" . $hero_description ."</div>";
+              }
+            ?>
+            <?php 
+              $text_primary_button = get_field('text_primary_button');
+              $link_primary_button = get_field('link_primary_button');
+
+              $text_play_button = get_field('text_play_button');
+              $source_link_video = get_field('source_link_video');
+
+              if( $text_primary_button ||  $text_play_button ) {
+                ?>
+                  <div class="buttons-banner mt-5">
+                    <?php 
+                      if($text_primary_button && $link_primary_button) {
+                        ?>
+                        <a class="btn" href="<?php echo esc_url($link_primary_button); ?>"><?php echo esc_html($text_primary_button)?></a> 
+                        <?php
+                      }
+                      if( $text_play_button && $source_link_video ) {
+                        ?>
+                        <a class="btn btn-play" href="<?php echo esc_url($source_link_video); ?>"><?php echo esc_html($text_play_button)?></a> 
+                        <?php
+                      }
+                    ?>
+                  </div>
+                <?php
+              }
+            ?>
+          </div>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
