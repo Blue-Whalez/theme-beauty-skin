0558d49016cf6f09b3beb28ec45de1ea1f4816a4
TuanTQ <tquoctua97@gmail.com>
2021-04-04 17:05:53 +0700

    refactor template hero, service

--- template-parts/hero.php
+++ template-parts/hero.php
@@ -1,7 +1,6 @@
 <?php 
     
     // Get ACF Fields of Hero Section
-
     $hero_title =  get_field('hero_title');
     $hero_description = get_field('hero_description');
     $hero_image = get_field('hero_image');
@@ -17,9 +16,7 @@
 <?php
 
     // Background Hero
-
     if( $hero_image ) {
-        
         ?>
         <style>
             #hero {
@@ -28,79 +25,51 @@
         </style>
         <?php
     }
-
 ?>
 
 <section class="hero py" id="hero" >
-
     <div class="row">
-
         <div class="col large-6">
 
-
             <?php if ( $hero_title ) : ?>
-
                 <h1 class="hero__heading"><?php echo $hero_title; ?></h1>
-
             <?php endif; ?>
-
 
             <?php if ( $hero_title ) : ?>
-
                 <div class="hero__description"><?php echo $hero_description; ?></div>
-
             <?php endif; ?>
 
-            <?php 
+            <?php  if( $text_primary_button ||  $text_play_button ) : ?>
+                <div class="buttons-banner mt-5">
 
-                if( $text_primary_button ||  $text_play_button ) {
-                    
-                    ?>
-                        <div class="buttons-banner mt-5">
+                    <?php if($text_primary_button && $link_primary_button) : ?> 
+                        <a class="btn btn-style btn--large " href="<?php echo esc_url($link_primary_button); ?>"> 
+                            <?php echo esc_html($text_primary_button)?>
+                        </a> 
+                    <?php endif; ?>
 
-                            <?php if($text_primary_button && $link_primary_button) : ?> 
-                            
-                                <a class="btn btn-style btn--large " href="<?php echo esc_url($link_primary_button); ?>"> 
+                    <?php if( $text_play_button && $source_link_video ) : ?>
+                        <a class="btn btn-play btn--large " id="openModalBox" href="#">
+                            <i class="fa fa-play-circle" aria-hidden="true"></i> <?php echo esc_html($text_play_button)?>
+                        </a> 
+                    <?php endif; ?>
 
-                                    <?php echo esc_html($text_primary_button)?>
-
-                                </a> 
-
-                            <?php endif; ?>
-
-
-                            <?php if( $text_play_button && $source_link_video ) : ?>
-
-                                <a class="btn btn-play btn--large " id="openModalBox" href="#">
-
-                                    <i class="fa fa-play-circle" aria-hidden="true"></i> <?php echo esc_html($text_play_button)?>
-
-                                </a> 
-
-                            <?php endif; ?>
-
-                        </div>
-
-                    <?php
-                }
-            ?>
+                </div>
+            <?php endif; ?>
 
         </div>
-
       </div>
-
 </section>
 
 <!-- Box Modal -->
-
+<?php if( $source_link_video ) : ?>
 <div class="modal" id="modalBox">
-
     <div class="modal__content">
-
         <span class="modal__close" id="closeModalBox">&times;</span>
 
         <?php echo $source_link_video; ?>         
 
     </div>
-
 </div>
+<?php endif; ?>
+
--- template-parts/service.php
+++ template-parts/service.php
@@ -1,17 +1,19 @@
 <?php 
 $service_section = get_field('service_section');
 if ( $service_section ) :
+    $heading = $service_section['heading'];
+    $description = $service_section['description'];
 ?>
-    <section class="cta py" id="cta">
+    <section class="services py" id="services">
         <div class="row ">
             <div class="col text-center mb-5" style="max-width: 800px; margin: 0 auto;">
 
-                <?php if( $service_section['heading'] ) : ?>
-                    <h2 class="heading-2 title-style"><?php echo $service_section['heading']; ?></h2>
+                <?php if( $heading ) : ?>
+                    <h2 class="heading-2 title-style"><?php echo esc_html( $heading ); ?></h2>
                 <?php endif; ?>
 
-                <?php if( $service_section['description'] ) : ?>
-                    <p class="m-0"><?php echo $service_section['description']; ?></p>
+                <?php if( $description ) : ?>
+                    <p class="m-0"><?php echo esc_html($description); ?></p>
                 <?php endif; ?>
 
             </div>
